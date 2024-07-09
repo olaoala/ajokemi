beda91537b8a786da3f90bca365d668f1707dfcb
wuraola babalola <babalolawuraola321@gmail.com>
2024-07-09 13:21:48 +0100

    deploy fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import Navbar from '../Common/Navbar';
 import Work from '../Component/Work';
 import About from '../Component/About';
-import { useScroll } from 'framer-motion';
 import Landing from '../Component/Landing';
 
 import Personals from '../Component/Personal';
@@ -19,7 +18,6 @@ const event = {
 
   const googleUrl ='https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ3eMcM5BC8Zz71h5pR6CLAWW3jBnNj9i0dGqHPzoMKm_pNJOFn7AFXKI8RWKzGBXDBDvvMXHmTr'
 const Home = () => {
-    const containerRef = useRef(null);
 
     return (
         <div className='font-playwrite text-white' >
@@ -31,7 +29,7 @@ const Home = () => {
             <Personals/>
             <div className='grid gap-4 m-10   items-center justify-center'>
                 <p className='text-xl text-gray-600'>Let's work together!</p>
-                <a href={googleUrl} target='_blank' className='text-white border  rounded-lg bg-blue text-center  p-4 text-sm font-bold'>Schedule a call</a>
+                <a href={googleUrl} target='_blank' rel="noopener noreferrer" className='text-white border  rounded-lg bg-blue text-center  p-4 text-sm font-bold'>Schedule a call</a>
             </div>
         </div>
     );
